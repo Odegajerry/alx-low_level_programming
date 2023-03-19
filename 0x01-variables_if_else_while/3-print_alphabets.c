413ec36328905fffe8acf6c4d94787938a444d44
Odegajerry <odegajerry@gmail.com>
2023-03-19 15:09:01 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,8 @@ int main(void)
 	char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
-		putchar(c);
-	for (c = 'A' ; c <= 'Z' ; c++)
-		putchar(c);
+		if (c != 'q' && c != 'e')
+		       putchar(c);
 	putchar('\n');
 	return (0);
 }
