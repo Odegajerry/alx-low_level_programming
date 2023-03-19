fb8cbbed26f354c727653d12cc8728979dad063f
Odegajerry <odegajerry@gmail.com>
2023-03-19 16:03:43 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		for (d = 1 ; d < 10 ; d++)
+		{
+			if (c < d || c != d)
+			{
+				putchar(c + '0');
+				putchar(d + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
