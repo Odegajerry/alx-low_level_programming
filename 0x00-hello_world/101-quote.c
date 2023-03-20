8f00b54a71ccf67aa89a81712412da116eccccb5
Odegajerry <odegajerry@gmail.com>
2023-03-20 12:08:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,8 @@
  *
  * Return: 1
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Kor
-		apar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", stdout);
 return (1);
 }
