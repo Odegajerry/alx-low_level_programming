24fe2ac391d244782ee1e9cb56223913b6289072
Odegajerry <odegajerry@gmail.com>
2023-03-20 12:31:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - A progrm that prints a line to the std error
  *
- * Return: Always 0
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
 	return (1);
 }
