5f45447f9dd6b0c727798c0bd975e0d1d41a2d9e
Odegajerry <odegajerry@gmail.com>
2023-03-20 12:04:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 /**
  * main - A progrm that prints a line to the std error
  *
- * Return 1
+ * Return: 1
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Kor
+		apar, 2015-10-19\n", stdout);
 return (1);
 }
