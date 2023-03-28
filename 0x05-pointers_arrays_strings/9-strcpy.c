229bd65b6da60e57546f868ec1180477f05bb7e3
Odegajerry <odegajerry@gmail.com>
2023-03-28 13:00:50 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + l) != '\0')
+	while (*(src + 1) != '\0')
 	{
 		l++;
 	}
-	for ( ; x < l; x++)
+	for ( ; x < 1; x++)
 	{
 		dest[x] = src[x];
 	}
